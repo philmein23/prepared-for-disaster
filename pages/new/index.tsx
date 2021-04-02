9df54b10b5cf98a6b2a01bfc059d4b19cb8a6ac0
Phillip Nguyen <phillip.d.nguyen23@gmail.com>
2021-04-02 10:26:04 -0700

    styling: styling form inputs

--- pages/new/index.tsx
+++ pages/new/index.tsx
@@ -1,12 +1,37 @@
+import { useForm } from 'react-hook-form';
 
 import style from './index.module.css';
 
+interface Inputs {
+    firstName: string;
+    lastName: string;
+    address: string;
+}
 
 const NewRegistration = () => {
+    const { register, handleSubmit } = useForm<Inputs>();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
     return (
-        <div>
-            Content
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label htmlFor="first-name">First Name</label>
+                <input type="text" id="first-name" {...register('firstName', { required: true })} />
+            </div>
+            <div>
+                <label htmlFor="last-name">Last Name</label>
+                <input type="text" id="last-name" {...register('lastName', { required: true })} />
+            </div>
+            <div>
+                <label htmlFor="address">Address</label>
+                <textarea id="address" {...register('address', { required: true})}></textarea>
+            </div>
+
+            <button type="submit">Submit</button>
+        </form>
     )
 }
 
